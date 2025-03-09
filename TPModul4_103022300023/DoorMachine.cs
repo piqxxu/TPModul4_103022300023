5f426c3a0bc99fee0ea73d7e249fcdaaeced3dfe
Muhammad Azwa Radya Razadisya <137063182+piqxxu@users.noreply.github.com>
2025-03-09 14:01:08 +0700

    Menambahkan implementasi dengan state-based construction

---
+++ TPModul4_103022300023/DoorMachine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPModul4_103022300023
+{
+    public class DoorMachine
+    {
+        private enum State { Terkunci, Terbuka }
+        private State currentState;
+
+        public DoorMachine()
+        {
+            currentState = State.Terkunci; 
+            Console.WriteLine("Pintu terkunci");
+        }
+
+        public void KunciPintu()
+        {
+            if (currentState == State.Terkunci)
+            {
+                Console.WriteLine("\nPintu sudah terkunci");
+            }
+            else
+            {
+                currentState = State.Terkunci;
+                Console.WriteLine("\nPintu terkunci");
+            }
+        }
+
+        public void BukaPintu()
+        {
+            if (currentState == State.Terbuka)
+            {
+                Console.WriteLine("\nPintu sudah terbuka");
+            }
+            else
+            {
+                currentState = State.Terbuka;
+                Console.WriteLine("\nPintu tidak terkunci");
+            }
+        }
+    }
+}
---
+++ TPModul4_103022300023/DoorMachineProgram.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace TPModul4_103022300023
+//{
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            DoorMachine door = new DoorMachine();
+
+//            while (true)
+//            {
+//                Console.WriteLine("Buka : Membuka pintu\nKunci : Mengunci pintu\nExit : Keluar\n");
+//                Console.Write("Input: ");
+//                string command = Console.ReadLine().ToLower();
+
+//                if (command == "buka")
+//                    door.BukaPintu();
+//                else if (command == "kunci")
+//                    door.KunciPintu();
+//                else if (command == "exit")
+//                    break;
+//                else
+//                    Console.WriteLine("\nMasukkan input yang sesuai");
+//            }
+//        }
+//    }
+//}
